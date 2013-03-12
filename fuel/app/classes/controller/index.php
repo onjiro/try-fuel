156012bbd6beeb4419513f80e3b163d286a04dd8
onjiro <onjiro.mohyahya@gmail.com>
2013-03-12 23:52:13 +0900

    Add REST action `get_list`

--- fuel/app/classes/controller/index.php
+++ fuel/app/classes/controller/index.php
@@ -1,6 +1,6 @@
 <?php
 
-class Controller_Index extends Controller_Template
+class Controller_Index extends Controller_Hybrid
 {
 
 	public function action_index()
@@ -9,4 +9,14 @@ class Controller_Index extends Controller_Template
 		$this->template->content = View::forge('index/index');
 	}
 
+  public function get_list()
+  {
+    return $this->response(array(
+                                 'foo' => Input::get('foo'),
+                                 'baz' => array(
+                                                1, 50, 219
+                                                ),
+                                 'empty' => null,
+                                 ));
+  }
 }
