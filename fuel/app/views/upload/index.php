ffcd90320b043192de127899c496d702f9be72e0
onjiro <onjiro.mohyahya@gmail.com>
2013-03-12 23:27:11 +0900

    Create initial upload form

--- fuel/app/views/upload/index.php
+++ fuel/app/views/upload/index.php
@@ -1,1 +1,13 @@
-<p>Index</p>
+<form class="form-horizontal">
+  <div class="control-group">
+    <label class="control-label">Select file</label>
+    <div class="controls">
+      <input type="file"/>
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <input type="submit" value="Upload" />
+    </div>
+  </div>
+</form>
